a901899ca972d36e7f3a20f92940fa8da19b28d0
yacchin1205 <yazawa@yzwlab.net>
2021-11-12 07:56:27 +0900

    Fix messages

--- R/syncToRDM.R
+++ R/syncToRDM.R
@@ -1,6 +1,6 @@
 syncToRDM <- function() {
   if (!file.exists('~/result')) {
-    message('~/result directory not exists')
+    message('~/result directory not exists.\n')
     return(FALSE)
   }
   jh_env = fromJSON('~/.config/grdm/env.json')
@@ -8,8 +8,15 @@ syncToRDM <- function() {
   jh_server_name = jh_env['JUPYTERHUB_SERVER_NAME']
   yyyymmdd = format(Sys.time(), '%Y%m%d')
   to_path = sprintf('/mnt/rdm/osfstorage/result-%s-%s-%s/', jh_user, yyyymmdd, jh_server_name)
+  cat(sprintf('Syncing...: ~/result -> %s\n', to_path))
   if (!dir.exists(to_path)) {
     dir.create(to_path)
   }
-  return(file.copy(from = list.files('~/result/', full.names = TRUE), to = to_path, recursive = TRUE))
+  r = file.copy(from = list.files('~/result/', full.names = TRUE), to = to_path, recursive = TRUE)
+  if (r) {
+    cat('Completed.\n')
+  } else {
+    message('Failed.\n')
+  }
+  return(r)
 }
